09704ca28d44f1e3acdca9ee805b001a1715119b
ShamshodOdinayev <shamshododinayev1702@gmail.com>
2024-01-22 22:01:27 +0500

    1-commit

---
+++ src/main/java/com/example/entity/ProfileEntity.java
@@ -0,0 +1,33 @@
+package com.example.entity;
+
+import com.example.enums.ProfileRole;
+import com.example.enums.ProfileStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "profile")
+public class ProfileEntity extends BaseEntity {
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private ProfileStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private ProfileRole role;
+}
---
+++ src/main/java/com/example/util/MD5Util.java
@@ -0,0 +1,21 @@
+package com.example.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Util {
+    public static String encode(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(password.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (byte b : array) {
+                sb.append(Integer.toHexString((b & 0xFF) | 0x100), 1, 3);
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
